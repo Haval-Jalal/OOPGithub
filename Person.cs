9e9ec432ff6a98baf3bb898cd5da479bfb8cfaf3
Haval-Jalal <Haval.jalal@hotmail.com>
2025-09-13 14:11:27 +0200

    Skapat en klass som heter Person. Satt värden och anropat en metod.

---
+++ Person.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace OOPGithub
+{
+    //Jag ska skapa en klass som heter Person, och där ska jag ha två attributer, Name och Age.
+    //Ska även ha en motod som heter Introduce som skriver ut "Hello, my name is [Name] and I am [Age] years old."
+    public class Person
+    {
+        public string Name;
+        public int Age;
+
+        public void Introduce()
+        {
+            Console.WriteLine($"Hello, my name is {Name} and I am {Age} years old.");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Har skapat en instans av klassen Person, och satt värdena för Name och Age, och sedan anropar jag metoden Introduce för att skriva ut informationen.
+
+            Person person = new Person();
+            person.Name = "Haval";
+            person.Age = 34;
+            person.Introduce();
+
         }
     }
 }
