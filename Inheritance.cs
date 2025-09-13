890b247addd8f27c8a85382fb993f0fa15e8e0d8
Haval-Jalal <Haval.jalal@hotmail.com>
2025-09-13 14:23:57 +0200

    Jag har skapat en ny klass som heter Inhertance och ärver från klassen Person. Jag har även satt in värden och anropat.

---
+++ Inheritance.cs
@@ -0,0 +1,14 @@
+﻿namespace OOPGithub
+{
+
+    // Jag ska skapa en klass som heter Inheritance som ärver från klassen Person.
+    // Den ska ha ett attribut som heter JobTitle och en metod som heter Work som skriver ut "I am working as a [JobTitle]."
+    public class Inheritance : Person
+    {
+        public string JobTitle;
+        public void Work()
+        {
+            Console.WriteLine($"I am working as a {JobTitle}.");
+        }
+    }
+}
